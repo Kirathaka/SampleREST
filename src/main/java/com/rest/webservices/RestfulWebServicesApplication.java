291f3a9caf606a11144a04db4721917d46227356
Chetan Raj <cnrj007@gmail.com>
2019-08-16 02:47:49 +0530

    added buildspec.yml and made changes for war deployment

--- src/main/java/com/rest/webservices/RestfulWebServicesApplication.java
+++ src/main/java/com/rest/webservices/RestfulWebServicesApplication.java
@@ -4,16 +4,23 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
-public class RestfulWebServicesApplication {
+public class RestfulWebServicesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RestfulWebServicesApplication.class);
+    }
 	
 	@Bean
 	public LocaleResolver localeResolver() {
